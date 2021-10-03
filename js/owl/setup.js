d20257999a3d21c3df269cdda826fd07c031abdc
Marcelo dos Santos <mdssjc@gmail.com>
2021-10-02 21:20:03 -0300

    feat: add modal

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -1,16 +1,29 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-})
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
+
+window.addEventListener("load", () => {
+  new jBox("Modal", {
+    attach: "#watchButton",
+    content: $("#watchModal"),
+  });
+
+  new jBox("Modal", {
+    attach: "#infoButton",
+    getTitle: "data-title",
+    content: $("#infoModal"),
+  });
+});
